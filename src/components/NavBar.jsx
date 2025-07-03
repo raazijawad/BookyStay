304c2dec2171efeb528bd70c3bdcceb3de73b9ce
raazijawad <mohdraazi105@gmail.com>
2025-07-03 00:43:59 -0700

    useClerk , useUser imported

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import { assets } from '../assets/assets';
+import { useClerk, useUser, UserButton } from '@clerk/clerk-react';
 const NavBar = () => {
     const navLinks = [
         { name: 'Home', path: '/' },
@@ -12,6 +13,9 @@ const NavBar = () => {
 
     const [isScrolled, setIsScrolled] = React.useState(false);
     const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+    const {openSignIn} = useClerk ()
+    const {user} = useUser ()
 
     React.useEffect(() => {
         const handleScroll = () => {
@@ -45,7 +49,7 @@ const NavBar = () => {
             {/* Desktop Right */}
             <div className="hidden md:flex items-center gap-4">
                <img src={assets.searchIcon} alt="search" className={`${isScrolled && 'invert'} h-7 transition-all duration-500`} />
-                <button className="bg-black text-white px-8 py-2.5 rounded-full ml-4 transition-all duration-500">
+                <button onClick={()} className="bg-black text-white px-8 py-2.5 rounded-full ml-4 transition-all duration-500">
                     Login
                 </button>
             </div>
