5467c8a65344b0aabbe5d4183644c182c2024d4b
raazijawad <mohdraazi105@gmail.com>
2025-07-04 01:56:32 -0700

    added css for the img tag

--- src/components/HotelCard.jsx
+++ src/components/HotelCard.jsx
@@ -5,8 +5,8 @@ import { assets } from '../assets/assets'
 const HotelCard = ({room, index}) => {
   return (
     <Link to={'/rooms/' + room._id} onClick={() => scrollTo(0,0)} key={room._id}>
-        <img src={room.images[0]} alt="" />
-        <p className='px-3 py-1 absolute top-3 left-3 text-xs bg-white text-gray-800 font-medium rounded-full'>Best seller</p>
+        <img src={room.images[0]}  alt="" className='relative max-w-70 w-full rounded-xl overflow-hidden bg-white text-gray-500/90 shadow-[0px_4px_4px_rgba(0,0,0,0.05)]'/>
+        {index % 2 === 0 &&<p className='px-3 py-1 absolute top-3 left-3 text-xs bg-white text-gray-800 font-medium rounded-full'>Best seller</p>}
         <div className='p-4 pt-4'>
             <div className='flex items-center justify-between'>
                 <p className='font-playfair text-xl font-medium text-gray-800'>{room.hotel.name}</p>
