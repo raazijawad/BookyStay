5ee7f0ec2bb9bd1657082eae5309dda9bd2c09f8
raazijawad <mohdraazi105@gmail.com>
2025-07-04 01:34:16 -0700

    star icone added

--- src/components/HotelCard.jsx
+++ src/components/HotelCard.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { assets } from '../assets/assets'
 
 const HotelCard = ({room, index}) => {
   return (
     <Link to={'/rooms/' + room._id} onClick={() => scrollTo(0,0)} key={room._id}>
+        <img src={room.images[0]} alt="" />
+        <p>Best seller</p>
+        <div>
+            <div>
+                <p className='font-playfair text-xl font-medium text-gray-800'>{room.hotel.name}</p>
+                <div className='flex items-center gap-1'>
+                    <img src={assets.starIconFilled} alt="" />4.5
+                </div>
+            </div>
+        </div>
     </Link>
   )
 }
