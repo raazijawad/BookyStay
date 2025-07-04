f853dff41e4d04816f4d9fd8bd57856b6000a311
raazijawad <mohdraazi105@gmail.com>
2025-07-04 01:28:36 -0700

    onclick funtion added

--- src/components/HotelCard.jsx
+++ src/components/HotelCard.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const HotelCard = () => {
+const HotelCard = ({room, index}) => {
   return (
-    <div>
-      
-    </div>
+    <Link to={'/rooms/' + room._id} onClick={() => scrollTo(0,0)} key={room._id}>
+    </Link>
   )
 }
 
