69d1f8ffec0d8c8aea0a13b9a631df0e0d604ce1
raazijawad <mohdraazi105@gmail.com>
2025-07-05 03:31:36 -0700

    Location icon added

--- src/components/HotelCard.jsx
+++ src/components/HotelCard.jsx
@@ -11,7 +11,7 @@ const HotelCard = ({room, index}) => {
             <div className='flex items-center justify-between'>
                 <p className='font-playfair text-xl font-medium text-gray-800'>{room.hotel.name}</p>
                 <div className='flex items-center gap-1'>
-                    <img src={assets.locationFilledIcon} alt="" />4.5
+                    <img src={assets.locationIcon} alt="" />4.5
                 </div>
             </div>
             <div>
