f268892130cf5cabc0538be463aaefbe7bd90926
raazijawad <mohdraazi105@gmail.com>
2025-07-05 04:28:28 -0700

    Featured destination mounted

--- src/components/FeaturedDestination.jsx
+++ src/components/FeaturedDestination.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { roomsDummyData } from '../assets/assets'
+import HotelCard from './HotelCard'
 
 const FeaturedDestination = () => {
   return (
     <div>
-      
+        <div>
+            {roomsDummyData.slice(0.4).map((room, index)=>(
+                <HotelCard key={room._id} room={room} index={index} />
+            ))}
+        </div>
     </div>
   )
 }
