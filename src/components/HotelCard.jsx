c768e40fc1976b7f6af7f323f7c5c39a02aea1de
raazijawad <mohdraazi105@gmail.com>
2025-07-05 04:02:31 -0700

    addded css

--- src/components/HotelCard.jsx
+++ src/components/HotelCard.jsx
@@ -14,9 +14,9 @@ const HotelCard = ({room, index}) => {
                     <img src={assets.locationIcon} alt="" />4.5
                     <span>{room.hotel.address}</span>
                 </div>
-                <div>
-                    <p><span>${room.pricePerNight}</span>/night</p>
-                    <button className='px-4 py-2 text-sm'>Book Now</button>
+                <div className='flex items-center justify-between mt-4'>
+                    <p><span className='text-xl text-gray-800'>${room.pricePerNight}</span>/night</p>
+                    <button className='px-4 py-2 text-sm font-medium border border-gray-300 rounded hover:bg-gray-50 transition-all cursor-pointer'>Book Now</button>
                 </div>
             </div>
             <div>
