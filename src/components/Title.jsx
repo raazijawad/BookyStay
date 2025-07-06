aa6145c5591c1201182e7cec73e1236cf682ed9d
raazijawad <mohdraazi105@gmail.com>
2025-07-06 07:16:36 -0700

    added css for the Parental DIV

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Title = ({title, subTitle, align, font}) => {
   return (
-    <div>
+    <div className={`flex flex-col justify-center items-center text-center`}>
       <h1 className={`text-4xl md:text-[40px] ${font}`}>{title}</h1>
       <p className='text-sm md:text-base text-gray-500/90 mt-2 max-w-174'>{subTitle}</p>
     </div>
