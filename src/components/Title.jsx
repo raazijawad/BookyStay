2d4a6c97ee6b2283fe3e18e33066eca1f7871d99
raazijawad <mohdraazi105@gmail.com>
2025-07-06 07:14:55 -0700

    added css for the

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Title = ({title, subTitle, align, font}) => {
   return (
     <div>
-      <h1 className=''>{title}</h1>
+      <h1 className={`text-4xl md:text-[40px]`}>{title}</h1>
       <p>{subTitle}</p>
     </div>
   )
