bb5515622d827bc03cfbf83242798545398645e9
raazijawad <mohdraazi105@gmail.com>
2025-07-06 07:04:58 -0700

    added title and subtitles

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const Title = () => {
+const Title = ({title, subTitle, align, font}) => {
   return (
     <div>
-      
+      <h1 className=''>{title}</h1>
+      <p>{subTitle}</p>
     </div>
   )
 }
