e76abe61403a335bab578b5d078683e03c74fc95
raazijawad <mohdraazi105@gmail.com>
2025-07-06 07:15:13 -0700

    added css for the Sub Title

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Title = ({title, subTitle, align, font}) => {
   return (
     <div>
-      <h1 className={`text-4xl md:text-[40px]`}>{title}</h1>
-      <p>{subTitle}</p>
+      <h1 className={`text-4xl md:text-[40px] ${font}`}>{title}</h1>
+      <p className='text-sm md:text-base text-gray-500/90 mt-2 max-w-174'>{subTitle}</p>
     </div>
   )
 }
