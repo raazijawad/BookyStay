d32a6f0dbd8ed4fbe99bc6ee40dffd7ad767228e
raazijawad <mohdraazi105@gmail.com>
2025-07-09 13:03:07 -0700

    Title added

--- src/components/ExclusiveOffer.jsx
+++ src/components/ExclusiveOffer.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Title from 'Title'
 
 const ExclusiveOffer = () => {
   return (
     <div>
       <div>
-        
+        <Title />
       </div>
     </div>
   )
