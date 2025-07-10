348bbe90d9676d27f46573c5deca3bc0cd0625a0
raazijawad <mohdraazi105@gmail.com>
2025-07-10 10:22:52 -0700

    item. description added

--- src/components/ExclusiveOffer.jsx
+++ src/components/ExclusiveOffer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {assets} from '../assets/assets'
+import {assets, exclusiveOffers} from '../assets/assets'
 import Title from './Title'
 
 
@@ -7,7 +7,7 @@ import Title from './Title'
 const ExclusiveOffer = () => {
   return (
     <div className='flex flex-col items-center px-6 md:px-16 lg:px-24 xl:px-32 pt-20 pb-30'>
-      <div className='flex flex-col md:flex-row items-center justify-center w-full'>
+      <div className='flex flex-col md:flex-row items-center justify-between w-full'>
         <Title align='left' title='Exclusive offers' subTitle='Take advantage of our limited-time offers and special packages to enhance your stay and create unforgettable memories.' />
         <button className='group flex items-center gap-2 font-medium cursor-pointer max-md:mt-12'>
           View all offer
@@ -15,8 +15,18 @@ const ExclusiveOffer = () => {
           className='group-hover:translate-x-1 transition-all' />
         </button>
       </div>
+
+      <div>
+        {exclusiveOffers.map((item)=>(
+          <div key={item._id} className='group relative flex flex-col items-start justify-between gap-1 pt-12 md:pt-18 px-4 rounded-xl text-white bg-no-repeat bg-cover bg-center' style={{backgroundImage: `url(${item.image})`}}>
+
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
 
 export default ExclusiveOffer
+
+
