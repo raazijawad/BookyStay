5d579dfbf5f7346c40d882414222820a7f099f56
raazijawad <mohdraazi105@gmail.com>
2025-07-11 09:02:55 -0700

    item description added

--- src/components/ExclusiveOffer.jsx
+++ src/components/ExclusiveOffer.jsx
@@ -21,7 +21,7 @@ const ExclusiveOffer = () => {
           <div key={item._id} className='group relative flex flex-col items-start justify-between gap-1 pt-12 md:pt-18 px-4 rounded-xl text-white bg-no-repeat bg-cover bg-center' style={{backgroundImage: `url(${item.image})`}}>
             <p className='px-3 py-1 absolute top-4 left-4 font-medium text-xs bg-white text-gray-800 rounded-full'>{item.priceOff}% OFF</p>
             <div>
-              <p>{item.title}</p>
+              <p className=''>{item.title}</p>
             </div>
           </div>
         ))}
