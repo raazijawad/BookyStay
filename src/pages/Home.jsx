56830ad906a3f82965728f3e86724d93a55709e7
raazijawad <mohdraazi105@gmail.com>
2025-07-12 10:28:55 -0700

    Testimonials page mounted into Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import FeaturedDestination from '../components/FeaturedDestination'
 import ExclusiveOffer from '../components/ExclusiveOffer'
+import Testimonials from '../components/Testimonials'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
         <Hero />
         <FeaturedDestination/>
         <ExclusiveOffer/>
+        <Testimonials/>
     </>
   )
 }
