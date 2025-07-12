9e4aa82b5ea2bccfea0b5ea30a8e800169556a53
raazijawad <mohdraazi105@gmail.com>
2025-07-12 10:28:27 -0700

    Titile added

---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Title from './Title'
+
+const Testimonials = () => {
+  return (
+    <div>
+        <Title title="What Our Guest Say"/>
+    </div>
+  )
+}
+
+export default Testimonials
