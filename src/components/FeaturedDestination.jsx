e0c0db3fac2377e20832e6b2212a84d4f3e4f8dd
raazijawad <mohdraazi105@gmail.com>
2025-07-20 13:54:09 -0700

    updated

--- src/components/FeaturedDestination.jsx
+++ src/components/FeaturedDestination.jsx
@@ -5,7 +5,7 @@ import Title from './Title'
 import { useNavigate } from 'react-router-dom'
 
 const FeaturedDestination = () => {
-
+//
   const navigate = useNavigate()
   return (
     <div className='flex flex-col items-center px-6 md:px-16 lg:px-24 bg-slate-50 py-20'>
