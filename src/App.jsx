3aeb7f9ff6f0193a12f4c3e803bdde8f8f7bdcbf
raazijawad <mohdraazi105@gmail.com>
2025-07-28 14:02:31 -0700

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import NavBar from './components/NavBar'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import HotelCard from './components/HotelCard'
+import Footer from './components/Footer'
+import AllRooms from './pages/AllRooms'
 
 const App = () => {
 
@@ -11,11 +13,13 @@ const App = () => {
   return (
     <div>
       {!isOwnerPath && <NavBar />}
-      <div className=''>
+      <div className='min-h-[70vh]'>
         <Routes>
           <Route path='/' element={<Home/>}/>
+          <Route path='/rooms' element={<AllRooms/>}/>
         </Routes>
       </div>
+              <Footer />
     </div>
   )
 }
