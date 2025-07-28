8f3cccfa02fca784dd8a2f97e1d7a38fa966d981
raazijawad <mohdraazi105@gmail.com>
2025-07-28 14:02:13 -0700

    updated

---
+++ src/pages/AllRooms.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { assets, roomsDummyData } from '../assets/assets'
+import { div } from 'framer-motion/client'
+import { useNavigate } from 'react-router-dom'
+import StarRating from '../components/StarRating'
+
+const AllRooms = () => {
+    const navigate = useNavigate();
+
+  return (
+    <div className='flex flex-col-reverse items-start justify-between pt-28 md:mt-35 px-4 md:px-16 lg:px-24 xl:px-32 lg:flex-row'>
+      <div>
+        <div className='flex flex-col items-start text-left'>
+            <h1 className='font-playfair text-4xl md:text-[40px]'>Hotel Rooms</h1>
+            <p className='text-sm md:text-base text-gray-500/90 mt-2 max-w-174'>Take advantage of our limited-time offers and special packages to enhance your stay and create unforgettable memories</p>
+        </div>
+
+        {roomsDummyData.map((room)=>(
+            <div>
+                <img onClick={()=> {navigate(`/rooms/${room._id}`); scrollTo(0,0)}} src={room.images[0]} alt="" title='View Rooms Detail' className='max-h-65 md:w-1/2 rounded-xl shadow-lg object-cover cursor-pointer'/>
+
+                <div className='md:w-1/2 flex flex-col gap-2'>
+                    <p className='text-gray-500'>{room.hotel.city}</p>
+                    <p onClick={()=> {navigate(`/rooms/${room._id}`); scrollTo(0,0)}}className='text-gray-800 text-3xl font-playfair cursor-pointer'>{room.hotel.name}</p>
+                    <div className='flex items-center'>
+                        <StarRating />
+                        <p className='ml-2'>200+ reviews</p>
+                    </div>
+                    <div className='flex items-center gap-1 text-gray-500 mt-2 text-sm'>
+                        <img src={assets.locationIcon} alt="" />
+                        <span>{room.hotel.address}</span>
+                    </div>
+                </div>
+            </div>
+        ))}
+
+        <div>
+
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default AllRooms
