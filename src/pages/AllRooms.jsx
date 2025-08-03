7d98b696111b82db00b5f2a8417cc2e91b3f362e
raazijawad <mohdraazi105@gmail.com>
2025-08-03 08:53:46 -0700

    filter mobile responsive added

--- src/pages/AllRooms.jsx
+++ src/pages/AllRooms.jsx
@@ -85,7 +85,7 @@ const AllRooms = () => {
       </div>
 
       {/* Filters */}
-      <div className='w-80 border border-gray-300 bg-white max-lg:mb-8 min-lg:mt-16'>
+      <div className='w-80 border border-gray-300 bg-white max-lg:mb-8 lg:mt-24 mt-15'>
         <div className={`flex items-center justify-between px-5 py-2.5 min-lg:border-b border-gray-300 ${openFilters && "border-b"}`}>
           <p className='text-base font-medium text-gray-800'>FILTERS</p>
           <div className='text-xs cursor-pointer'>
